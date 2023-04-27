5d14cc66072cfc5e27d8790dba69ce67ebe915c0
“simp1e” <A013425@A013425>
2023-04-27 17:02:45 +0800

    设置钱包连接按钮

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,47 @@
+import type { AppProps } from "next/app";
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
+import '@rainbow-me/rainbowkit/styles.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { infuraProvider } from 'wagmi/providers/infura'
+import { publicProvider } from 'wagmi/providers/public';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [
+    infuraProvider({ apiKey: 'https://mainnet.infura.io/v3/655dcda26fd949a295880de6a1f2940d' }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'simpledapp',
+  projectId: '0',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
+
+
